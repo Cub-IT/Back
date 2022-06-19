68a3d902abb18e26e8c51e45c8bc8f6454c1655b
Anikanov Aleksandr <anikanov.alexander@gmail.com>
2022-06-19 21:28:53 +0300

    user permission service WIP

--- src/main/java/com/cub/project/service/UserPermissionService.java
+++ src/main/java/com/cub/project/service/UserPermissionService.java
@@ -1,5 +1,6 @@
 package com.cub.project.service;
 
+import com.cub.project.domain.models.Group;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,24 +8,28 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserPermissionService {
     private UserService userService;
+    private GroupService groupService;
 
     public boolean isAuthenticated(long userId, String authUserLogin) {
-        return false;
+        if (userService.getUserById(userId) == null) {
+            return false;
+        }
+        return userService.getUserById(userId).equals(userService.getUserByEmail(authUserLogin));
+    }
+
+    public boolean isMember(long groupId, String authUserLogin) {
+        return userService.getUserByEmail(authUserLogin).getParticipants().stream().anyMatch(p -> p.getGroup().getId() == groupId);
     }
 
     public boolean userIsMember(long userId, long groupId, String authUserLogin) {
         if (isAuthenticated(userId, authUserLogin)) {
-            return true;
-        } else {
-            return false;
+            return isMember(groupId, authUserLogin);
         }
-    }
-
-    public boolean isMember(long groupId, String authUserLogin) {
         return false;
     }
 
     public boolean isAdmin(long groupId, String authUserLogin) {
-        return false;
+        Group group = groupService.getGroupById(groupId);
+        return group != null && group.getParticipants().stream().anyMatch(p -> p.getUser().getEmail().equals(authUserLogin));
     }
 }
